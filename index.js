fa22082dbcee21055ead32f78a11c249cd026f02
patricia <patricia.mwangi@student.moringaschool.com>
2024-03-30 04:28:31 +0300

    title casing

--- index.js
+++ index.js
@@ -10,7 +10,23 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
-const titleCased = () => {
-  return tutorials
+function toTitleCase(str){
+  return str.replace(/\b\w/g, function(c){
+    return c.toUpperCase();
+  }).replace(/([A-Z])/g, '$1').trim();
 }
+const titleCased = () => {
+  const unchangedWords=['OO',"JSONP?",'API'];
+  return tutorials.map(tutorial=>{
+const words= tutorial.split(' ')
+const tiltleCasedWords=words.map(toTitleCase,word=>{
+  if(unchangedWords.includes(word.toUpperCase()) || !isNaN(word))
+  {
+    return word;
+  }else{ return toTitleCase()
+  //return word.charAt(0).toUpperCase()+word.slice(1).toLowerCase() 
+  }
+});
+return tiltleCasedWords.join(' ')
+});
+  }
